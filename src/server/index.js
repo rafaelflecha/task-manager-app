3063e6099d00d30677af8fb0b8884e8f02c8c8d5
Rafael Flecha <rafael.flecha@gmail.com>
2018-07-08 22:08:00 -0300

    node process environment

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const publicweb = process.env.PUBLICWEB || './dist/publicweb';
+const publicweb = process.env.PUBLICWEB || './publicweb';
 const app = express();
 
 app.use(express.static(publicweb));
@@ -9,5 +9,5 @@ app.get('*', (req, res) => {
   res.sendFile(`index.html`, { root: publicweb });
 });
 
-const port = process.env.SERVER_PORT || '3000';
+const port = process.env.PORT || '3000';
 app.listen(port, () => console.log(`API running on localhost:${port}`));
